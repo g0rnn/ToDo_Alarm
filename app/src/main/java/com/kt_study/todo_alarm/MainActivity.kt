22d17add105405668dac751656b601aa1664d06d
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-07-03 16:42:37 +0900

    chore: 불필요한 import문 제거

--- app/src/main/java/com/kt_study/todo_alarm/MainActivity.kt
+++ app/src/main/java/com/kt_study/todo_alarm/MainActivity.kt
@@ -2,16 +2,7 @@ package com.kt_study.todo_alarm
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kt_study.todo_alarm.ui.theme.ToDo_AlarmTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
